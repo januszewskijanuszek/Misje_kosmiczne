5f93e37f21795f74fa63ab92fe8553c40d54dd3e
januszewskijanuszek <januszewskijanuszek@gmail.com>
2024-05-10 18:00:16 +0200

    Code refractor to folders and base main structure

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
+/*
+* @file main.cpp
+* @author Patryk Januszewski
+* Desc:
+* Main executable file in program
+*/
+
+
 #include <iostream>
-#include <math.h>
+#include <cstdint>
+#include <string>
 
 using namespace std;
 
-int main(){
-    int value = pow(10, 0.5);
-    cout << value << endl;
+int main(int argc, char* argv[]){
+    uint32_t v_value = 0;
+
+    // Loop through command line arguments
+    for(int i = 1 ; i < argc ; i++){
+        string arg = argv[i];
+        if(arg == "-v"){
+            if(i + 1 < argc){
+                v_value = stoi(argv[i + 1]);
+                i++;
+            } else {
+                cerr << "Error: -v flag requires a value." << endl;
+                return 1;
+            }
+        }
+    }
+    cout << "Value of -v flag: " << v_value << endl;
     return 0;
 }
