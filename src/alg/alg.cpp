65938f1a8b0eb97c05b034c21447bb1cd30d58f5
januszewskijanuszek <januszewskijanuszek@gmail.com>
2024-06-06 22:42:01 +0200

    anomalia prawdziwa

--- src/alg/alg.cpp
+++ src/alg/alg.cpp
@@ -15,8 +15,6 @@ double anomaliaSrednia(map_T *input_data, map_T *input_date_data){
     return input_data->at("m0") + n * tk(input_data, input_date_data);
 }
 
-// -- LTS --
-
 double anomaliaMimosr(const double &anomalia_sr, const double &e,const uint32_t iteracions = 100){
     double Ek = 0;
     for(uint32_t i = 0 ; i < iteracions ; i++)
@@ -24,13 +22,18 @@ double anomaliaMimosr(const double &anomalia_sr, const double &e,const uint32_t 
     return Ek;
 }
 
+// -- LTS --
+
+double anomaliaPrawdziwa(const double &momosrod, const double &e){
+    return (2 * atan2(sqrt(1 + e) * sin(momosrod / 2), sqrt(1 - e) * cos(momosrod / 2)));
+}
+
 map_T internal::getX_Y(map_T *input_data, map_T *input_date_data){
+    const double &e = input_data -> at("e");
+
     double abomalia_sr = anomaliaSrednia(input_data, input_date_data);
-    double anomalia_mimosr = anomaliaMimosr(abomalia_sr, input_data -> at("e"));
-    double sin = 0.0;
-    double cos = 0.0;
-    double nu = 0.0;
-    double anomalia_praw = 0.0;
+    double anomalia_mimosr = anomaliaMimosr(abomalia_sr, e);
+    double nu = anomaliaPrawdziwa(anomalia_mimosr, e);
     double anomalia_praw_pery = 0.0;
     double u_popraw = 0.0;
     double r_popraw = 0.0;
@@ -42,7 +45,7 @@ map_T internal::getX_Y(map_T *input_data, map_T *input_date_data){
     double Y = 0.0;
     double Z = 0.0;
 
-    std::cout << anomalia_mimosr << std::endl;
+    std::cout << nu << std::endl;
 
     return {{"X", 1.0},{"Y", 1.0},{"Z", 1.0}};
 }
